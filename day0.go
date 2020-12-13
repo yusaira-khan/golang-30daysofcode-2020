c2a9d9940157085429fa9d540bd76dfae3657850
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-13 13:02:30 -0500

    finish day0

--- day0.go
+++ day0.go
@@ -1,10 +1,13 @@
 package main
-import "fmt"
-
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 func main() {
 	//Enter your code here. Read input from STDIN. Print output to STDOUT
 	fmt.Println("Hello, World.")
-	var first string
-	fmt.Scanln(&first)
-	fmt.Println(first)
+	reader := bufio.NewReader(os.Stdin)
+	text, _ := reader.ReadString('\n')
+	fmt.Print(text)
 }
