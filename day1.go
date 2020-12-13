37679c05eeada1c5124a6f4a2d3e92cdc4ce5533
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-13 13:24:17 -0500

    day1 scaffolding

---
+++ day1.go
@@ -0,0 +1,31 @@
+package main
+
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strconv"
+)
+
+func main() {
+	var _ = strconv.Itoa // Ignore this comment. You can still use the package "strconv".
+
+	var i uint64 = 4
+	var d float64 = 4.0
+	var s string = "HackerRank "
+
+	scanner := bufio.NewScanner(os.Stdin)
+	// Declare second integer, double, and String variables.
+
+	// Read and save an integer, double, and String to your variables.
+
+	// Print the sum of both integer variables on a new line.
+
+	// Print the sum of the double variables on a new line.
+
+	// Concatenate and print the String variables on a new line
+	// The 's' variable above should be printed first.
+
+
+}
