7f469e4aca448f3aaf16ab44f49ae4740d7fe3f1
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-13 12:58:05 -0500

    add day0 code

---
+++ day0.go
@@ -0,0 +1,10 @@
+package main
+import "fmt"
+
+func main() {
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	fmt.Println("Hello, World.")
+	var first string
+	fmt.Scanln(&first)
+	fmt.Println(first)
+}
